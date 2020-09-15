528dacda9056085fc447dba9f4ebf86d34bbf9a8
nva12 <nvallee.a7@gmail.com>
2020-09-15 16:20:03 +0400

    complete modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,12 +2,15 @@ import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 let mobileMenu = new MobileMenu();
 let stickyHeader = new StickyHeader();
 
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
+
+new Modal();
 
 if (module.hot) {
   module.hot.accept();
